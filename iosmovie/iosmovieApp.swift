7dbfc1a79268ce56d10c8b87aadd355ebe2e9bc8
Dfm007 <724625725@qq.com>
2026-08-25 19:08:48 +0800

    回退CLPlayer方案，切回ZFPlayer播放器

--- iosmovie/iosmovieApp.swift
+++ iosmovie/iosmovieApp.swift
@@ -1,19 +1,10 @@
 ﻿import SwiftUI
-import UIKit
 
 @main
 struct iosmovieApp: App {
-    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
-
     var body: some Scene {
         WindowGroup {
             HomeView()
         }
-    }
-}
-
-final class AppDelegate: NSObject, UIApplicationDelegate {
-    func application(_ application: UIApplication, supportedInterfaceOrientationsFor window: UIWindow?) -> UIInterfaceOrientationMask {
-        return .allButUpsideDown
     }
 }
